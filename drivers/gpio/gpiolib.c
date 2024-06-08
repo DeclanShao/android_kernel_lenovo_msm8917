5754e33ba83079c25d9afb5df9b3c2d8740a6726
DeclanShao <74823397+DeclanShao@users.noreply.github.com>
2024-06-08 10:26:06 +0000

    drivers: gpio: Add lenovo tb8704x,tb8704v gpio changes

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -2254,12 +2254,24 @@ static int _gpiod_direction_output_raw(struct gpio_desc *desc, int value)
 		pr_debug("[GPIO]set GPIO_65 as irq output\n");
 	else {
 #endif
+
+#if defined(CONFIG_MACH_LENOVO_TB8704X) || defined(CONFIG_MACH_LENOVO_TB8704V)
+		if (test_bit(FLAG_USED_AS_IRQ, &desc->flags)) {
+			gpiod_err(desc,
+				"%s: tried to set a GPIO tied to an IRQ as output\n",
+				__func__);
+			if(desc_to_gpio(desc) != 65) {
+				return -EIO;
+			}
+		}
+#else
 		if (test_bit(FLAG_USED_AS_IRQ, &desc->flags)) {
 			gpiod_err(desc,
 				 "%s: tried to set a GPIO tied to an IRQ as output\n",
 				 __func__);
 			return -EIO;
 		}
+#endif
 #ifdef CONFIG_TOUCHSCREEN_GT9XX_v24
 	}
 #endif
